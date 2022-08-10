ad473b6f4db4e4cd86c71104042ca0f7bb2d9ed3
Discusser <47938380+Discusser@users.noreply.github.com>
2022-08-10 22:19:41 +0200

    Made it compatible with linux

--- extractSounds.py
+++ extractSounds.py
@@ -7,20 +7,20 @@ def main():
     try:
         version: str = input("What version do you want to extract sounds from? (1.7.10, 1.8, 1.9, 1.10, 1.11, 1.12, "
                              "1.13.1, 1.14, 1.15, 1.16, 1.17, 1.18, 1.19) ")
-        version1 = re.match(r'\d.\d*', version)[0]
-        if version1 == "1.7":
-            version1 = "1.7.10"
-        elif version1 == "1.13":
-            version1 = "1.13.1"
-        assets: str = open("assets\\indexes\\" + version + ".json").read()
+        version = re.match(r'\d.\d*', version)[0]
+        if version == "1.7":
+            version = "1.7.10"
+        elif version == "1.13":
+            version = "1.13.1"
+        assets: str = open("assets/indexes/" + version + ".json").read()
         cwd = os.getcwd()
-        destination = cwd + "\\extractedSounds\\" + version
+        destination = cwd + "/extractedSounds/" + version
         print("Extracting sounds from " + version + " to " + destination)
         os.makedirs(destination)
         for match in re.finditer(r'((?<=sounds/).*?\.ogg).*?((?<=ogg": {"hash": ").*?(?="))', assets):
             file = match.group(1)
             _hash = match.group(2)
-            shutil.copyfile(cwd + "\\assets\\objects\\" + _hash[:2] + "\\" + _hash, destination + "\\" +
+            shutil.copyfile(cwd + "/assets/objects/" + _hash[:2] + "/" + _hash, destination + "/" +
                             os.path.basename(file))
         print("Finished extracting sounds")
         input()
